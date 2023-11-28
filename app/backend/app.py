5df38b466fac3f1e1c91cb96656789b69a0bc33a
Matt Gotteiner <matthew.gotteiner@microsoft.com>
2023-11-27 16:38:12 -0800

    refactor

--- app/backend/app.py
+++ app/backend/app.py
@@ -5,7 +5,8 @@ import mimetypes
 import os
 import time
 from pathlib import Path
-from typing import AsyncGenerator
+from typing import AsyncGenerator, cast
+import dataclasses
 
 import aiohttp
 import openai
@@ -30,8 +31,10 @@ from quart import (
 )
 from quart_cors import cors
 
+from approaches.approach import Approach
 from approaches.chatreadretrieveread import ChatReadRetrieveReadApproach
 from approaches.retrievethenread import RetrieveThenReadApproach
+from approaches.retrievethenreadvision import RetrieveThenReadVisionApproach
 from core.authentication import AuthenticationHelper
 
 CONFIG_OPENAI_TOKEN = "openai_token"
@@ -39,6 +42,7 @@ CONFIG_CREDENTIAL = "azure_credential"
 CONFIG_VISION_KEY = "vision_key"
 CONFIG_VISION_ENDPOINT = "vision_endpoint"
 CONFIG_ASK_APPROACH = "ask_approach"
+CONFIG_ASK_VISION_APPROACH = "ask_vision_approach"
 CONFIG_CHAT_APPROACH = "chat_approach"
 CONFIG_BLOB_CONTAINER_CLIENT = "blob_container_client"
 CONFIG_AUTH_CLIENT = "auth_client"
@@ -113,7 +117,12 @@ async def ask():
     auth_helper = current_app.config[CONFIG_AUTH_CLIENT]
     context["auth_claims"] = await auth_helper.get_auth_claims_if_enabled(request.headers)
     try:
-        approach = current_app.config[CONFIG_ASK_APPROACH]
+        use_gpt4v = context.get("overrides", {}).get("use_gpt4v", False)
+        approach: Approach
+        if use_gpt4v and CONFIG_ASK_VISION_APPROACH in current_app.config:
+            approach = cast(Approach, current_app.config[CONFIG_ASK_VISION_APPROACH])
+        else:
+            approach = cast(Approach, current_app.config[CONFIG_ASK_APPROACH])
         # Workaround for: https://github.com/openai/openai-python/issues/371
         async with aiohttp.ClientSession() as s:
             openai.aiosession.set(s)
@@ -125,11 +134,16 @@ async def ask():
         logging.exception("Exception in /ask: %s", error)
         return jsonify(error_dict(error)), 500
 
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
 
 async def format_as_ndjson(r: AsyncGenerator[dict, None]) -> AsyncGenerator[str, None]:
     try:
         async for event in r:
-            yield json.dumps(event, ensure_ascii=False) + "\n"
+            yield json.dumps(event, ensure_ascii=False, cls=JSONEncoder) + "\n"
     except Exception as e:
         logging.exception("Exception while generating response stream: %s", e)
         yield json.dumps(error_dict(e))
@@ -279,8 +293,6 @@ async def setup_clients():
         OPENAI_HOST,
         AZURE_OPENAI_CHATGPT_DEPLOYMENT,
         OPENAI_CHATGPT_MODEL,
-        AZURE_OPENAI_GPT4V_DEPLOYMENT,
-        AZURE_OPENAI_GPT4V_MODEL,
         AZURE_OPENAI_EMB_DEPLOYMENT,
         OPENAI_EMB_MODEL,
         KB_FIELDS_SOURCEPAGE,
@@ -288,6 +300,21 @@ async def setup_clients():
         AZURE_SEARCH_QUERY_LANGUAGE,
         AZURE_SEARCH_QUERY_SPELLER,
     )
+
+    if AZURE_OPENAI_GPT4V_MODEL:
+        current_app.config[CONFIG_ASK_VISION_APPROACH] = RetrieveThenReadVisionApproach(
+            search_client,
+            blob_container_client,
+            OPENAI_HOST,
+            AZURE_OPENAI_GPT4V_DEPLOYMENT,
+            AZURE_OPENAI_GPT4V_MODEL,
+            AZURE_OPENAI_EMB_DEPLOYMENT,
+            OPENAI_EMB_MODEL,
+            KB_FIELDS_SOURCEPAGE,
+            KB_FIELDS_CONTENT,
+            AZURE_SEARCH_QUERY_LANGUAGE,
+            AZURE_SEARCH_QUERY_SPELLER,
+        )
 
     current_app.config[CONFIG_CHAT_APPROACH] = ChatReadRetrieveReadApproach(
         search_client,
--- app/backend/approaches/approach.py
+++ app/backend/approaches/approach.py
@@ -1,17 +1,76 @@
 from dataclasses import dataclass
-from typing import Any, AsyncGenerator, Optional, Union
+from typing import Any, AsyncGenerator, Optional, Union, List, cast, Type
+from azure.search.documents.aio import SearchClient
+from azure.search.documents.models import QueryType, Vector, CaptionResult
+from quart import current_app
+import openai
+import logging
+import aiohttp
 
 from core.authentication import AuthenticationHelper
 
+@dataclass
+class Document:
+    id: Optional[str]
+    content: Optional[str]
+    embedding: Optional[List[float]]
+    image_embedding: Optional[List[float]]
+    category: Optional[str]
+    sourcepage: Optional[str]
+    sourcefile: Optional[str]
+    oids: Optional[List[str]]
+    groups: Optional[List[str]]
+    captions: List[CaptionResult]
+
+    def serialize_for_results(self) -> dict[str, Any]:
+        return {
+            "id": self.id,
+            "content": self.content,
+            "embedding": Document.trim_embeddings(self.embedding),
+            "imageEmbedding": Document.trim_embeddings(self.image_embedding),
+            "category": self.category,
+            "sourcepage": self.sourcepage,
+            "sourcefile": self.sourcefile,
+            "oids": self.oids,
+            "groups": self.groups,
+            "captions": [{"additional_properties": caption.additional_properties, "text": caption.text, "highlights": caption.highlights} for caption in self.captions] if self.captions else []
+        }
+
+    @classmethod
+    def trim_embeddings(cls, embedding: Optional[List[float]]) -> Optional[str]:
+        if embedding:
+            if len(embedding) > 2:
+                #Format the embedding list to show the first 2 items followed by the count of the remaining items."""
+                return f"[{embedding[0]}, {embedding[1]} ...+{len(embedding) - 2} more]"
+            else:
+                return str(embedding)
+
+        return None
 
 @dataclass
-class ThoughtStep:
+class ThoughtStep():
     title: str
-    description: Any | str | None
+    description: Optional[Any]
     props: Optional[dict[str, Any]] = None
 
 
 class Approach:
+    def __init__(
+        self,
+        search_client: SearchClient,
+        query_language: Optional[str],
+        query_speller: Optional[str],
+        embedding_deployment: Optional[str],  # Not needed for non-Azure OpenAI or for retrieval_mode="text"
+        embedding_model: str,
+        openai_host: str,
+    ):
+        self.search_client = search_client
+        self.query_language = query_language
+        self.query_speller = query_speller
+        self.embedding_deployment = embedding_deployment
+        self.embedding_model = embedding_model
+        self.openai_host = openai_host
+
     def build_filter(self, overrides: dict[str, Any], auth_claims: dict[str, Any]) -> Optional[str]:
         exclude_category = overrides.get("exclude_category") or None
         security_filter = AuthenticationHelper.build_security_filters(overrides, auth_claims)
@@ -22,6 +81,47 @@ class Approach:
             filters.append(security_filter)
         return None if len(filters) == 0 else " and ".join(filters)
 
+    async def search(self, top: int, query_text: Optional[str], filter: Optional[str], vectors: List[Vector],  use_semantic_ranker: bool, use_semantic_captions: bool) -> List[Document]:
+        # Use semantic ranker if requested and if retrieval mode is text or hybrid (vectors + text)
+        if use_semantic_ranker and query_text:
+            results = await self.search_client.search(
+                search_text=query_text,
+                filter=filter,
+                query_type=QueryType.SEMANTIC,
+                query_language=self.query_language,
+                query_speller=self.query_speller,
+                semantic_configuration_name="default",
+                top=top,
+                query_caption="extractive|highlight-false" if use_semantic_captions else None,
+                vectors=vectors,
+            )
+        else:
+            results = await self.search_client.search(search_text=query_text or "", filter=filter, top=top, vectors=vectors)
+
+        documents = []
+        async for page in results.by_page():
+            async for document in page:
+                documents.append(Document(id=document.get("id"), content=document.get("content"), embedding=document.get("embedding"), image_embedding=document.get("imageEmbedding"), category=document.get("category"), sourcepage=document.get("sourcepage"), sourcefile=document.get("sourcefile"), oids=document.get("oids"), groups=document.get("groups"), captions=cast(List[CaptionResult], document.get("@search.captions"))))
+        return documents
+
+    async def compute_text_embedding(self, q: str):
+        embedding_args = {"deployment_id": self.embedding_deployment} if self.openai_host == "azure" else {}
+        embedding = await openai.Embedding.acreate(**embedding_args, model=self.embedding_model, input=q)
+        return Vector(value=embedding["data"][0]["embedding"], k=50, fields="embedding")
+
+    async def compute_image_embedding(self, q: str):
+        endpoint = f"{current_app.config['vision_endpoint']}computervision/retrieval:vectorizeText"
+        params = {"api-version": "2023-02-01-preview", "modelVersion": "latest"}
+        headers = {"Content-Type": "application/json", "Ocp-Apim-Subscription-Key": current_app.config['vision_key']}
+        data = {"text": q}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=endpoint, params=params, headers=headers, json=data) as response:
+                response.raise_for_status()
+                json = await response.json()
+                image_query_vector = json["vector"]
+        return Vector(value=image_query_vector, k=50, fields="imageEmbedding")
+    
     async def run(
         self, messages: list[dict], stream: bool = False, session_state: Any = None, context: dict[str, Any] = {}
     ) -> Union[dict[str, Any], AsyncGenerator[dict[str, Any], None]]:
